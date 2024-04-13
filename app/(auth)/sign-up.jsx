5924c9428d99ceeadbe5f5564c81ffd8e3f74da4
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2024-04-13 22:34:11 +0500

    Update sign-up.jsx

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -7,6 +7,7 @@ import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 import { Link, router } from "expo-router";
 import { createUser } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const SignUp = () => {
   const [form, setForm] = useState({
@@ -16,8 +17,10 @@ const SignUp = () => {
   });
   const [isSubmitting, SetIsSubmitting] = useState(false);
 
+  const { setUser, setIsLoggedIn } = useGlobalContext();
+
   const submit = async () => {
-    if (!form.username || !form.email || !form.password) {
+    if (form.username === '' || form.email === '' || form.password === '') {
       Alert.alert('Error', "Please fill in all the fields");
     }
 
@@ -25,8 +28,8 @@ const SignUp = () => {
 
     try {
       const result = await createUser(form.email, form.password, form.username);
-
-      // set it to global state using context
+      setUser(result)
+      setIsLoggedIn(true)
 
       router.replace('/home');
     } catch (error) {
