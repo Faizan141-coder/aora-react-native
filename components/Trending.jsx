5ce45e91c776fcee93b20a8844e45f7c9340f16b
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2024-04-14 20:28:51 +0500

    Update Trending.jsx

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -8,6 +8,8 @@ import {
   Image,
 } from "react-native";
 import * as Animatable from "react-native-animatable";
+import { Video, ResizeMode } from "expo-av";
+
 import { icons } from "../constants";
 
 const zoomIn = {
@@ -38,7 +40,18 @@ const TrendingItem = ({ activeItem, item }) => {
       duration={500}
     >
       {play ? (
-        <Text className="text-white">PLaying</Text>
+        <Video 
+          source={{ uri: item.video }}
+          className='w-52 h-72 rounded-[35px] mt-3 bg-white/10'
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if(status.didJustFinish) {
+              setPlay(false)
+            }
+          }}
+        />
       ) : (
         <TouchableOpacity
           className="relative justify-center items-center"
